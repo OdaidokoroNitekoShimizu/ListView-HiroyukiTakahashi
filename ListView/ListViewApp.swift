06553268e14399aec8b8c3a35697080938ec4d2c
Hiroyuki <lahm0107@gmail.com>
2025-11-02 10:53:19 +0900

    Initial Commit

---
+++ ListView/ListViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListViewApp.swift
+//  ListView
+//
+//  Created by hiroyuki takahashi on R 7/11/02.
+//
+
+import SwiftUI
+
+@main
+struct ListViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
